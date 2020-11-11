14b8d7954fd6d2e67e76b115fc1b7262cc0242b2
danielescillia <daniele.scillia@gmail.com>
2020-11-11 22:52:47 +0100

    Solved first test with CONSTANT (step 3 of TPP)

--- tests/Kata/RnaTranscriptionTest.php
+++ tests/Kata/RnaTranscriptionTest.php
@@ -7,20 +7,18 @@ use Kata\RnaTranscription;
 
 class RnaTranscriptionTest extends TestCase
 {
-    private RnaTranscription $rnaTranscription;
+    private $rnaTranscription;
 
     protected function setUp(): void
     {
         $this->rnaTranscription = new RnaTranscription();
     }
 
-    public function testShallPass(): void
+    public function testDnaGIsTranscribedToRnaC(): void
     {
-        $this->assertEquals(1, 1);
+        $actual = $this->rnaTranscription->handle('G');
+
+        $this->assertEquals('C', $actual);
     }
 
-    public function testHandleReturnTrue(): void
-    {
-        $this->assertEquals(true, $this->rnaTranscription->handle());
-    }
 }
