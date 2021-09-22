0e85cbae95355a55bfc31270757cd3b7066dc2b2
danielescillia <daniele.scillia@gmail.com>
2021-09-22 11:59:32 +0200

    chore: reset

--- tests/Kata/RnaTranscriptionTest.php
+++ tests/Kata/RnaTranscriptionTest.php
@@ -7,20 +7,12 @@ use Kata\RnaTranscription;
 
 class RnaTranscriptionTest extends TestCase
 {
-    private RnaTranscription $rnaTranscription;
+    private $rnaTranscription;
 
     protected function setUp(): void
     {
         $this->rnaTranscription = new RnaTranscription();
     }
 
-    public function testShallPass(): void
-    {
-        $this->assertEquals(1, 1);
-    }
 
-    public function testHandleReturnTrue(): void
-    {
-        $this->assertEquals(true, $this->rnaTranscription->handle());
-    }
 }
