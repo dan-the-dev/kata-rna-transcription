189c9f2525293480a652a491e8c35dceb8064c6e
danielescillia <daniele.scillia@gmail.com>
2021-09-22 12:08:47 +0200

    feat: implemented second test with Obvious Implementation strategy

--- src/Kata/RnaTranscription.php
+++ src/Kata/RnaTranscription.php
@@ -5,8 +5,11 @@ namespace Kata;
 class RnaTranscription
 {
 
-    public function transcribe(string $string)
+    public function transcribe(string $dnaStrand): string
     {
+        if ($dnaStrand === 'C') {
+            return 'G';
+        }
         return 'C';
     }
 }
