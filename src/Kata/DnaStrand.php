612d36b6f833e8d1815fef227e7536cd433b7575
danielescillia <daniele.scillia@gmail.com>
2021-09-22 12:20:24 +0200

    refactor: improved easiness of usage for DnaStrand object by adding builders methods to encapsulate possible values

--- src/Kata/DnaStrand.php
+++ src/Kata/DnaStrand.php
@@ -4,12 +4,31 @@ namespace Kata;
 
 class DnaStrand
 {
+    const CYTOSINE = 'C';
+    const GUANINE = 'G';
+    const THYMINE = 'T';
+
     /** @var string $strand */
     private $strand;
 
     public function __construct(string $strand)
     {
         $this->strand = $strand;
+    }
+
+    public static function cytosine(): DnaStrand
+    {
+        return new DnaStrand(self::CYTOSINE);
+    }
+
+    public static function guanine(): DnaStrand
+    {
+        return new DnaStrand(self::GUANINE);
+    }
+
+    public static function thymine(): DnaStrand
+    {
+        return new DnaStrand(self::THYMINE);
     }
 
     public function equals(DnaStrand $anotherDnaStrand): bool
